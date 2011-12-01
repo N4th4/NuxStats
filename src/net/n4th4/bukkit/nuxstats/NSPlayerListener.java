70e9a47cd5ec0f4416c9816f6f22ed9338fb57d1
Nathanaël Restori <nathanael@n4th4.net>
2011-12-01 17:48:46 +0100

    Add the players' name in a file

--- src/net/n4th4/bukkit/nuxstats/NSPlayerListener.java
+++ src/net/n4th4/bukkit/nuxstats/NSPlayerListener.java
@@ -1,8 +1,13 @@
 package net.n4th4.bukkit.nuxstats;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerListener;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.json.simple.JSONArray;
 
 public class NSPlayerListener extends PlayerListener {
     private final NuxStats plugin;
@@ -11,13 +16,26 @@ public class NSPlayerListener extends PlayerListener {
         plugin = instance;
     }
 
+    @SuppressWarnings("unchecked")
     public void onPlayerJoin(PlayerJoinEvent event) {
-        plugin.playersCount++;
-        plugin.writePlayersNumber();
+        JSONArray json = new JSONArray();
+        List<Player> list = Arrays.asList(plugin.getServer().getOnlinePlayers());
+        for (int i = 0; i < list.size(); i++) {
+            json.add(list.get(i).getName());
+        }
+        plugin.write("playersName.txt", json.toJSONString());
+        plugin.write("playersNumber.txt", Integer.toString(json.size()));
     }
 
+    @SuppressWarnings("unchecked")
     public void onPlayerQuit(PlayerQuitEvent event) {
-        plugin.playersCount--;
-        plugin.writePlayersNumber();
+        JSONArray json = new JSONArray();
+        List<Player> list = Arrays.asList(plugin.getServer().getOnlinePlayers());
+        for (int i = 0; i < list.size(); i++) {
+            if (!event.getPlayer().getName().equals(list.get(i).getName()))
+                json.add(list.get(i).getName());
+        }
+        plugin.write("playersName.txt", json.toJSONString());
+        plugin.write("playersNumber.txt", Integer.toString(json.size()));
     }
 }
--- src/net/n4th4/bukkit/nuxstats/NuxStats.java
+++ src/net/n4th4/bukkit/nuxstats/NuxStats.java
@@ -6,12 +6,16 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Logger;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.Event.Priority;
 import org.bukkit.event.Event;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.json.simple.JSONArray;
 
 public class NuxStats extends JavaPlugin {
     private final NSPlayerListener playerListener = new NSPlayerListener(this);
@@ -22,16 +26,23 @@ public class NuxStats extends JavaPlugin {
         try {
             new File("plugins/NuxStats/").mkdirs();
             new File("plugins/NuxStats/playersNumber.txt").createNewFile();
+            new File("plugins/NuxStats/playersName.txt").createNewFile();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    @SuppressWarnings("unchecked")
     public void onEnable() {
         log = this.getServer().getLogger();
 
-        playersCount = getServer().getOnlinePlayers().length;
-        writePlayersNumber();
+        JSONArray json = new JSONArray();
+        List<Player> list = Arrays.asList(getServer().getOnlinePlayers());
+        for (int i = 0; i < list.size(); i++) {
+            json.add(list.get(i).getName());
+        }
+        write("playersName.txt", json.toJSONString());
+        write("playersNumber.txt", Integer.toString(json.size()));
 
         PluginManager pm = getServer().getPluginManager();
         pm.registerEvent(Event.Type.PLAYER_JOIN, playerListener, Priority.Normal, this);
@@ -39,17 +50,17 @@ public class NuxStats extends JavaPlugin {
     }
 
     public void onDisable() {
-        playersCount = 0;
-        writePlayersNumber();
+        write("playersNumber.txt", "[]");
+        write("playersNumber.txt", Integer.toString(0));
     }
-
-    public void writePlayersNumber() {
+    
+    public void write(String file, String text) {
         try {
-            OutputStreamWriter osw = new OutputStreamWriter(new BufferedOutputStream(new FileOutputStream("plugins/NuxStats/playersNumber.txt")), "8859_1");
-            osw.write(String.valueOf(playersCount));
+            OutputStreamWriter osw = new OutputStreamWriter(new BufferedOutputStream(new FileOutputStream("plugins/NuxStats/" + file)), "8859_1");
+            osw.write(text);
             osw.close();
         } catch (FileNotFoundException e) {
-            log.severe("[NuxStats] File not found : plugins/NuxStats/playersNumber.txt");
+            log.severe("[NuxStats] File not found : plugins/NuxStats/" + file);
         } catch (IOException e) {
             e.printStackTrace();
         }
